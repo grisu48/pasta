143c1aeb8fce9a954eea86e8266d41e661559a02
felix@feldspaten.org <phoenix>
2021-01-14 21:40:25 +0100

    Improved deploy script

--- deploy_pasta.sh
+++ deploy_pasta.sh
@@ -9,6 +9,22 @@ DATA="/srv/pasta"                # Data directory for config file and pastas
 PORT="8199"                      # Exposed port of the container
 
 
+function usage() {
+	echo "$0 - Simple pasta deployment script"
+	echo "Usage: $0 [DATADIR]         - deploy instance as docker container"
+	echo "       $0 --rm              - remove container"
+	echo ""
+	echo "OPTIONS"
+	echo "   DATADIR    - Directory where data will be stored"
+}
+
+if [[ $# -ge 1 ]]; then
+	DATA="$1"
+	if [[ $DATA == "-h" || $DATA == "--help" ]]; then
+		usage
+		exit 0
+	fi
+fi
 
 
 
@@ -16,7 +32,23 @@ if [[ $CONTAINER_NAME != "" ]]; then
 	docker container stop "$CONTAINER_NAME"
 	docker container rm "$CONTAINER_NAME"
 fi
+
+# Special use case: Remove container
+if [[ $DATA == "--rm" ]]; then
+	exit 0
+fi
+
 docker pull grisu48/pasta
+
+
+# Prepare data directory
+mkdir -p "$DATA"
+if [[ ! -s "$DATA/pastad.toml" ]]; then
+	echo "Using default configuration file $DATA/pastad.toml"
+	cp "pastad.toml.example" "$DATA/pastad.toml"
+else
+	echo "Found existing pastad.toml configuration"
+fi
 
 set -e
 
